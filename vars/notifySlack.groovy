b28dd57dcc2dc7cbee5a5727d7b0c6c8feee50be
Mayur Naithani <mayur.naithani@hotmail.com>
2020-11-17 17:17:13 +0530

    Add slack notificaion

---
+++ vars/notifySlack.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call(String buildResult,String stageName) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend channel: 'devops',color: 'good', message: "stageName within ${JOB_NAME} with buildnumber ${BUILD_NUMBER} has completed, check ${BUILD_URL}", teamDomain: 'projectdevops', tokenCredentialId: 'Slackcred'
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend channel: 'devops',color: 'danger', message: "stageName within ${JOB_NAME} with buildnumber ${BUILD_NUMBER} has failed, check ${BUILD_URL}", teamDomain: 'projectdevops', tokenCredentialId: 'Slackcred'
+  }
+  else {
+      slackSend channel: 'devops',color: 'warning', message: "status for ${JOB_NAME} with buildnumber ${BUILD_NUMBER} unclear, teamDomain: 'projectdevops', tokenCredentialId: 'Slackcred'
+  }
+}
